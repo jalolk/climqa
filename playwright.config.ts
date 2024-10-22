98cf5d0309ee6c632f3f3d63fc392e91c5479b77
Jalol Khamroev <jalol.khamroev@toptal.com>
2024-10-23 03:27:09 +0500

    feat(ci): setup ci for test and deploy

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,9 +74,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: "yarn run start",
-  //   url: "http://127.0.0.1:5173",
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npx serve dist -p 5173",
+    url: "http://127.0.0.1:5173",
+    timeout: 120_000,
+    reuseExistingServer: !process.env.CI,
+  },
 });
