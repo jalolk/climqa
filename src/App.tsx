ed6c208b2888522e50c2077b403068ef981e607b
Jalol Khamroev <jalol.khamroev@toptal.com>
2024-10-24 02:38:40 +0500

    feat(cd): applied the imporvements in CD

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <div className="min-h-screen bg-gradient-to-b from-blue-400 to-blue-600 p-4">
         <div className="max-w-4xl mx-auto">
           <h1 className="text-4xl font-bold text-white text-center mb-8">
-            ClimQa Weather App
+            ClimQa - Weather App
           </h1>
           <WeatherSearch />
         </div>
